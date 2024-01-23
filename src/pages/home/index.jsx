acf197647ae285cf64f088f3a5babc810f4102b1
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-22 21:35:32 -0300

    finalização layout

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -2,13 +2,23 @@ import './index.scss';
 import book from '../../../assets/Imagens/book.svg';
 const Home = () =>{
     return (
-        <div>
+        <div className='full'>
             <header className="header">
             <img src={book} alt="foto livro" />
             </header>
-            <h1 className='title_busca'>Qual livro você quer <br />conhecer hoje?</h1>
-            <input className='input_busca' placeholder='buscar livro...' type="text" /><br />
-            <button className='btn_busca'>BUSCAR</button>
+
+            <div className='title_busca_title'>
+            <h1 >Qual livro você quer <br />conhecer hoje?</h1>
+            </div>
+            <br />
+            <div className='input_busca' >
+            <input placeholder='buscar livro...' type="text" /><br />
+            </div>
+            <br />
+            <div className='btn_busca'>
+            <button >BUSCAR</button>
+            </div>
+            
             <div className='footer'>
             </div>
         </div>
