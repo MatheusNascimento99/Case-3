c75635178ee2f5a7da78e37885032b306821fc00
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2024-01-24 11:53:54 -0300

    finalizado lógica para busca de livros

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,64 +4,82 @@ import livros from '../../../mock/dataFake';
 import { useRef, useState } from 'react';
 
 const Home = () => {
-  const valorRef = useRef();  //*Ref para campo input
-  const [livroEscolhido, setLivroEscolhido] = useState([]);  
+    const valorRef = useRef();  //*Ref para campo input
+    const [livroEscolhido, setLivroEscolhido] = useState([]);
 
-  const pegarValor = () => {     //! função para pegar valor do input
-    const valorInput = valorRef.current.value;
 
-    
-    const livrosFiltrados = livros.filter( 
-      elemento =>
-        valorInput === elemento.nome ||
-        valorInput === elemento.autor ||
-        valorInput === elemento.categoria
-    );
+    const pegarValor = () => {     //! função para pegar valor do input
+        const valorInput = valorRef.current.value;
+        
 
-    
-    setLivroEscolhido(livrosFiltrados);
+        const livrosFiltrados = livros.filter(
+            elemento =>
+                valorInput === elemento.nome ||
+                valorInput === elemento.autor ||
+                valorInput === elemento.categoria
+        );
 
-    valorRef.current.value = '';
 
-  }
+        setLivroEscolhido(livrosFiltrados);
 
-  return (
-    <div className='full'>
-      <header className="header">
-        <img src={book} alt="foto livro" />
-      </header>
+        valorRef.current.value = ''
 
-      <div className='title_busca_title'>
-        <h1 >Qual livro você quer <br />conhecer hoje?</h1>
-      </div>
+    }
 
-      <br />
-      <div className='input_busca' >
-        <input placeholder='buscar livro...' ref={valorRef} type="text" /><br />
-      </div>
+    return (
+        <div className='full'>
+            <header className="search_header">
+                <img src={book} alt="foto livro" />
+            </header>
 
-      <br />
+            <div className='search'>
 
-      <div className='btn_busca'>
-        <button onClick={pegarValor} >BUSCAR</button>
-      </div>
 
-      <div className='footer'>
-      </div>
+                <div className='search_title'>
+                    <h1 >Qual livro você quer <br />conhecer hoje?</h1>
+                </div>
 
-      <div className='div_livros'>
-        {livroEscolhido.map((elemento, index) => (
-          <div key={index} className='livro'>
-            <h2>{elemento.nome}</h2>
-            <p>Autor: {elemento.autor}</p>
-            <p>Descrição: {elemento.descrição}</p>
-            <p>Categoria: {elemento.categoria}</p>
-            <img src={elemento.imagem} alt="error" />
-          </div>
-        ))}
-      </div>
-    </div>
-  )
+                <br />
+                <div className='search_input' >
+                    <input placeholder='buscar livro...' ref={valorRef} type="text" /><br />
+                </div>
+
+                <br />
+
+                <div className='search_btn'>
+                    <button onClick={pegarValor} >BUSCAR</button>
+                </div>
+
+            </div>
+            <div className='search_footer'>
+            </div>
+            <div className='change'>
+
+
+                <div className='div_livros'>
+                    {livroEscolhido.map((elemento, index) => (
+                        <div key={index} className='livro'>
+                            <img src={elemento.imagem} alt="error" />
+                            <div className='leitura'>
+                            <button id='ok'>Já Li!</button> <br /> <br />
+                            <button id='not'>Quero ler!</button>
+                            </div>
+                            
+                            <h2 className='livro_info'>{elemento.nome}</h2>
+                            <p className='livro_info'>{elemento.autor}</p>
+                            <p className='livro_info'><strong>Gênero:</strong> {elemento.categoria}</p>
+                            <p className='descricao'>{elemento.descrição}</p>
+
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+
+
+
+        </div>
+    )
 }
 
 export default Home;
